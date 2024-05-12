dc3134cce5d442855847c6365d44691b2765bc68
Dmitrii <erikfrish@proton.me>
2024-05-12 20:53:00 +0300

    .

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,2 @@
-sudo /opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/init.zip --server-settings /opt/factorio/data/server-settings.json
+# sudo /opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/init.zip --server-settings /opt/factorio/data/server-settings.json
+/opt/factorio/bin/x64/factorio --start-server-load-latest --server-settings /opt/factorio/data/server-settings.json
