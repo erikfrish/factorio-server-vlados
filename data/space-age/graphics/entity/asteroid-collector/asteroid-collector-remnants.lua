11eecb9fa3c1dce105b722288b3092e58a7ef008
erikfrish <erikfrish@proton.me>
2025-05-30 23:23:07 +0300

    gustosmyl vstupil v igru

---
+++ data/space-age/graphics/entity/asteroid-collector/asteroid-collector-remnants.lua
@@ -0,0 +1,7 @@
+return
+{
+  width = 316,
+  height = 324,
+  shift = util.by_pixel( 0.0, 19.5),
+  line_length = 1,
+}
---
+++ data/space-age/graphics/entity/crusher/remnants/crusher-remnants.lua
@@ -0,0 +1,7 @@
+return
+{
+  width = 254,
+  height = 210,
+  shift = util.by_pixel( -1.0, 5.5),
+  line_length = 1,
+}
