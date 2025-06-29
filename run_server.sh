ded79717f366a287368f42793a33b3583a6fa186
erikfrish <erikfrish@proton.me>
2025-06-29 16:02:12 +0000

    damn

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 # sudo /opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/init.zip --server-settings /opt/factorio/data/server-settings.json
-/opt/factorio/bin/x64/factorio --start-server-load-latest --server-settings /opt/factorio/data/server-settings.json --bind 0.0.0.0 --port 34197
+/opt/factorio/bin/x64/factorio --start-server-load-latest --server-settings /opt/factorio/data/server-settings.json --bind 0.0.0.0 --port 9999 
